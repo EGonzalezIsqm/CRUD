b12e893079a8c5dd48224aa176def2361bbd5085
EGonzalezIsqm <eegonzalez03@misena.edu.co>
2016-11-26 21:12:25 -0500

    Carga

---
+++ build/generated-sources/ap-source-output/com/prueba/entitis/Usuarios_.java
@@ -0,0 +1,17 @@
+package com.prueba.entitis;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-26T17:09:45")
+@StaticMetamodel(Usuarios.class)
+public class Usuarios_ { 
+
+    public static volatile SingularAttribute<Usuarios, String> apellido;
+    public static volatile SingularAttribute<Usuarios, String> equipo;
+    public static volatile SingularAttribute<Usuarios, String> documento;
+    public static volatile SingularAttribute<Usuarios, String> nombre;
+    public static volatile SingularAttribute<Usuarios, Integer> idtable1;
+
+}
---
+++ src/java/com/prueba/entitis/Usuarios.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.prueba.entitis;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author ADMIN
+ */
+@Entity
+@Table(name = "usuarios")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Usuarios.findAll", query = "SELECT u FROM Usuarios u"),
+    @NamedQuery(name = "Usuarios.findByIdtable1", query = "SELECT u FROM Usuarios u WHERE u.idtable1 = :idtable1"),
+    @NamedQuery(name = "Usuarios.findByNombre", query = "SELECT u FROM Usuarios u WHERE u.nombre = :nombre"),
+    @NamedQuery(name = "Usuarios.findByApellido", query = "SELECT u FROM Usuarios u WHERE u.apellido = :apellido"),
+    @NamedQuery(name = "Usuarios.findByDocumento", query = "SELECT u FROM Usuarios u WHERE u.documento = :documento"),
+    @NamedQuery(name = "Usuarios.findByEquipo", query = "SELECT u FROM Usuarios u WHERE u.equipo = :equipo")})
+public class Usuarios implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Basic(optional = false)
+    @Column(name = "idtable1")
+    private Integer idtable1;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "apellido")
+    private String apellido;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "documento")
+    private String documento;
+    @Size(max = 45)
+    @Column(name = "equipo")
+    private String equipo;
+
+    public Usuarios() {
+    }
+
+    public Usuarios(Integer idtable1) {
+        this.idtable1 = idtable1;
+    }
+
+    public Usuarios(Integer idtable1, String nombre, String apellido, String documento) {
+        this.idtable1 = idtable1;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.documento = documento;
+    }
+
+    public Integer getIdtable1() {
+        return idtable1;
+    }
+
+    public void setIdtable1(Integer idtable1) {
+        this.idtable1 = idtable1;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idtable1 != null ? idtable1.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuarios)) {
+            return false;
+        }
+        Usuarios other = (Usuarios) object;
+        if ((this.idtable1 == null && other.idtable1 != null) || (this.idtable1 != null && !this.idtable1.equals(other.idtable1))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.prueba.entitis.Usuarios[ idtable1=" + idtable1 + " ]";
+    }
+    
+}
